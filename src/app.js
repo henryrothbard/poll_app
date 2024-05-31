4a17cc848e9ad3b762b723f470c7e1b2e0717c2b
henryrothbard <henryrothbard24@gmail.com>
2024-05-31 18:05:59 -0400

    commit

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(logger('dev'));
-app.use(cors())
+app.use(cors({ credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
